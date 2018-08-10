0ecfa108586dbceabbacfae753aa827aca0dc6ca
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-08-10 15:27:34 -0700

    got tests to work

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -5,29 +5,14 @@ class HelloWorld extends Component {
     constructor(props){
         super(props);
         this.state = { greeting: 'Hello' };
+        this.frenchify = this.frenchify.bind(this);
+        this.italianify = this.italianify.bind(this);
+        this.germanify = this.germanify.bind(this);
         this.removeGreeting = this.removeGreeting.bind(this);
     }
     removeGreeting() {
         this.props.removeGreeting(this.props.name);
     }
-    render() {
-    return (
-        <div className='HelloWorld'>
-            {this.state.greeting} {this.props.name}!
-            <br/>
-            <div className='buttons'>
-                <button className='frenchify' onClick={this.frenchify.bind(this)}>Frenchify!</button>
-                <br/>
-                <button className='italianify' onClick={this.italianify.bind(this)}>Italianify!</button>
-                <br/>
-                <button className='germanify' onClick={this.germanify.bind(this)}>Germanify!</button>
-                <br/>
-                <button onClick={this.removeGreeting}>Remove Me!</button>
-            </div>    
-        </div>
-        );
-    }
-    
     frenchify(){
         this.setState({ greeting: 'Bonjour' });
     }
@@ -36,6 +21,23 @@ class HelloWorld extends Component {
     }
     germanify(){
         this.setState({ greeting: 'Guten Tag' })
+    }
+    render() {
+    return (
+        <div className='HelloWorld'>
+            {this.state.greeting} {this.props.name}!
+            <br/>
+            <div className='buttons'>
+                <button className='frenchify' onClick={this.frenchify}>Frenchify!</button>
+                <br/>
+                <button className='italianify' onClick={this.italianify}>Italianify!</button>
+                <br/>
+                <button className='germanify' onClick={this.germanify}>Germanify!</button>
+                <br/>
+                <button className='remove' onClick={this.removeGreeting}>Remove Me!</button>
+            </div>    
+        </div>
+        );
     }
 };
 
